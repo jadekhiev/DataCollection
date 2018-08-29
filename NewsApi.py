ff2caa4054f79ed839b49d39fc596f50c8a65f1d
Padmanie <padmanie.maulkhan@gmail.com>
2018-08-29 17:41:02 -0400

    Added pageloop and csv write
    Now we can loop through pages and write the results to one csv

--- NewsApi.py
+++ NewsApi.py
@@ -4,22 +4,21 @@
 
 
 #import newsapi
-import requests     
+import requests
+import csv
 
-def NewsFromBBC():
-
-    #j = 1   
+def NewsFromBBC(j):
+  
     # BBC news api
+    for i in range(1,j):
+        main_url = " https://newsapi.org/v2/everything?sources=bbc-news&pageSize=100&page=" + str(i) + "&apiKey=abd1cde781dc46b385045b20e214a7e8"   
+        # fetching data in json format
+             
+        articlesToCSV(main_url,i)
 
-   # for j in range(6):
-    main_url = " https://newsapi.org/v2/everything?sources=bbc-news&pageSize=100&page=2&apiKey=abd1cde781dc46b385045b20e214a7e8"
-    #main_url = "https://newsapi.org/v2/everything?country=ca&pageSize=100&page=1&apiKey=abd1cde781dc46b385045b20e214a7e8"
-    
-    
-    # fetching data in json format
-    open_bbc_page = requests.get(main_url).json()
-
+def articlesToCSV(main_url, k):
     # getting all articles in a string article
+    open_bbc_page = requests.get(main_url).json()  
     article = open_bbc_page["articles"]
 
     # empty list which will contain all trending news
@@ -31,14 +30,18 @@ def NewsFromBBC():
         titles.append(ar["title"])
         description.append(ar["description"])
         url.append(ar["url"])        
-         
-    for i in range(len(titles)):
-        # printing all trending news
-        print(i + 1, "|", titles[i], "|", description[i], "|",url[i] )                 
-       # print(i + 1, description[i])       
+
+             # writing all trending news to csv        
+    with open('articles.csv', 'a', newline='') as f:
+        articlewriter = csv.writer(f, delimiter = ',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i in range(len(titles)):
+            s = (((k-1)*100) + i + 1, titles[i], description[i], url[i] )
+            articlewriter.writerow(s)
+    f.close()
+    
 
 # Driver Code
 if __name__ == '__main__': 
     # function call
-    NewsFromBBC() 
+    NewsFromBBC(11) 
 
